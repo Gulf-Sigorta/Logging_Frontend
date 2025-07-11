54b370ea5452700951fe591ad5b0cdf8dcf9b070
Cakduruk <canakduruk@gmail.com>
2025-07-11 13:49:13 +0300

    Level'a göre filtre eklendi ve pagination eklendi

--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -4,17 +4,75 @@ import LogTable from '../components/LogTable/LogTable';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [page, setPage] = useState(0);      
+  const [totalPages, setTotalPages] = useState(0);
 
+  const levels = ['ERROR', 'WARN', 'INFO', 'DEBUG'];
+  const pageSize = 15; 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/log')
-      .then(response => setLogs(response.data))
-      .catch(error => console.error('Error fetching logs:', error));
-  }, []);
+    const fetchLogs = async () => {
+      try {
+        const baseUrl = selectedLevel 
+          ? `http://localhost:8080/api/log/get-log-by-level/${selectedLevel}`
+          : `http://localhost:8080/api/log`;
+
+        const url = `${baseUrl}?page=${page}&size=${pageSize}`;
+        const response = await axios.get(url);
+
+        setLogs(response.data.content);       
+        setTotalPages(response.data.totalPages); 
+      } catch (err) {
+        console.error('Loglar alınamadı:', err);
+      }
+    };
+    fetchLogs();
+  }, [selectedLevel, page]);
 
   return (
-    <div>
-      <h1>Logs</h1>
-      <LogTable logs={logs} />
+    <div style={{ padding: '20px' }}>
+      <h2>Loglar</h2>
+
+      <label>
+        Log Seviyesi:
+        <select
+          value={selectedLevel}
+          onChange={(e) => {
+            setSelectedLevel(e.target.value);
+            setPage(0);  // Seviye değişince sayfa 0'a dönsün
+          }}
+          style={{ marginLeft: '10px', padding: '5px' }}
+        >
+          <option value="">Tümü</option>
+          {levels.map((level, idx) => (
+            <option key={idx} value={level}>{level}</option>
+          ))}
+        </select>
+      </label>
+
+      <div style={{ marginTop: '20px' }}>
+        <LogTable logs={logs} />
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
+        <button 
+          onClick={() => setPage(page - 1)} 
+          disabled={page === 0}
+          style={{ marginRight: '10px' }}
+        >
+          Önceki
+        </button>
+
+        <span>Sayfa {page + 1} / {totalPages}</span>
+
+        <button 
+          onClick={() => setPage(page + 1)} 
+          disabled={page + 1 === totalPages}
+          style={{ marginLeft: '10px' }}
+        >
+          Sonraki
+        </button>
+      </div>
     </div>
   );
 };
